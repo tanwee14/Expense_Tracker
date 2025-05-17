3fbc87eb7097abec11b2804bee76be4d21429ad1
tanwee14 <8409tan@gmail.com>
2025-05-17 15:00:00 +0530

    Added connectDb.js

--- backend/config/connectDb.js
+++ backend/config/connectDb.js
@@ -5,7 +5,7 @@ const connectDb = async () => {
         await mongoose.connect(process.env.MONGO_URL);
         console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
     } catch (error) {
-        console.log(`${error}`.bgRed);
+        console.log(`${error}`.bgRed) ;
 
         
     }
