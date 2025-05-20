303c4c670794ddda2900282d5603029b97ed7d5b
tanwee14 <8409tan@gmail.com>
2025-05-20 15:00:00 +0530

    Added models folder and wrote basic user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+//scheema design
+const userSchema = new mongoose.Schema(
+    {
+    name:{
+        type:String,
+        required:[true,"name is required"],
+    },
+    email:{
+        type:String,
+        required:[true,"email is required and should be unique"],
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:[true,"password is required"],
+    },
+
+},
+{ timestamps: true }
+);
